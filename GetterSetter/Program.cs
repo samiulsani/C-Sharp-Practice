faf1a26b778a8895fe535f8bda0b1d224435b56a
samiulsani <samiulslm5@gmail.com>
2025-09-22 14:35:50 +0600

    Getter and setter

---
+++ GetterSetter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    //Gatter and Setter
+
+    class Income
+    {
+        private double salary; // field
+        public double Salary   // property
+        {
+            get { return salary; }   // getter
+            set
+            {
+                if (salary <= 0)
+                {
+                    salary = 0;
+                }
+                else
+                {
+                    salary = value;
+                }  // setter
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Income inc = new Income();
+                inc.Salary = 45000; // setter
+                Console.WriteLine("Salary: " + inc.Salary); // getter
+                inc.Salary -= 75000; // setter
+                Console.WriteLine("Updated Salary: " + inc.Salary); // getter
+
+            }
+        }
+    }
+}
---
+++ Getter_Setter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter_Setter
+{
+    internal class Program
+    {
+        class Income
+        {
+            private double salary; // Private field to store the salary
+            public double Salary
+            {
+                get { return salary; } // Getter to access the salary
+                set
+                {
+                    if (value >= 0) // Validation to ensure salary is non-negative
+                    {
+                        salary = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Salary cannot be negative");
+                    }
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Income income = new Income();
+                income.salary = 2000; // Using the setter to set the salary
+                Console.WriteLine("Salary: " + income.salary); // Using the getter to get the salary
+
+            }
+        }
+    }
+}
